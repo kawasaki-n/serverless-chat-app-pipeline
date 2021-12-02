a583e4824b6ef81d4babd941b0644f7ad0dec8bd
Naoya Kawasaki <qqks23sd@gmail.com>
2021-12-03 01:18:11 +0900

    Initial commit

---
+++ test/cdk-deploy.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as CdkDeploy from '../lib/cdk-deploy-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-deploy-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkDeploy.CdkDeployStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
